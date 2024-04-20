518ebd8ec1b50fd7fed141563fdb6cb48f241fd3
SandyVii <sandeep.kollipara@icloud.com>
2024-04-21 03:44:49 +0530

    Debugging III.

--- main.py
+++ main.py
@@ -35,19 +35,19 @@ if __name__ == '__main__':
     print('Socket now listening')
 
     while True:
-    client_socket,addr = server_socket.accept()
-    print('Connection from:',addr)
-    if client_socket:
-        vid = cv2.VideoCapture(0)
-        while(vid.isOpened()):
-            img,frame = vid.read()
-            a = pickle.dumps(frame)
-            message = struct.pack("Q",len(a))+a
-            client_socket.sendall(message)
-            cv2.imshow('Sending...',frame)
-            key = cv2.waitKey(10) 
-            if key ==13:
-                client_socket.close()
+        client_socket,addr = server_socket.accept()
+        print('Connection from:',addr)
+        if client_socket:
+            vid = cv2.VideoCapture(0)
+            while(vid.isOpened()):
+                img,frame = vid.read()
+                a = pickle.dumps(frame)
+                message = struct.pack("Q",len(a))+a
+                client_socket.sendall(message)
+                cv2.imshow('Sending...',frame)
+                key = cv2.waitKey(10) 
+                if key ==13:
+                    client_socket.close()
 
     input("Press ENTER to exit.")
     print("Bye!")
