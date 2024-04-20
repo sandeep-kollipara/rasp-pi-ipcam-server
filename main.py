fd418dcda4e636b774bcd44064db86bb1b914d2c
SandyVii <sandeep.kollipara@icloud.com>
2024-04-21 03:02:16 +0530

    Debugging I.

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from picamera2 import Picamera2
 if __name__ == '__main__':
 
     picam2 = Picamera2()
-    picam2..start_and_record_video("out/test.mp4", duration=5)
+    picam2.start_and_record_video("out/test.mp4", duration=5)
 
     input("Press ENTER to exit.")
     print("Bye!")
