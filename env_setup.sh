4f19d1ee1b50e1fcb6e60c2092ddb16d3ba77c0a
SandyVii <sandeep.kollipara@icloud.com>
2024-04-21 13:00:36 +0530

    Debugging IV.

--- env_setup.sh
+++ env_setup.sh
@@ -6,4 +6,5 @@ sudo apt install -y python3-libcamera python3-kms++
 sudo apt install -y python3-prctl libatlas-base-dev ffmpeg libopenjp2-7 python3-pip
 pip3 install numpy --upgrade
 NOGUI=1 pip3 install picamera2
-python -m pip install -r requirements_addl.txt
+pip3 install imutils
+#python -m pip install -r requirements_addl.txt
